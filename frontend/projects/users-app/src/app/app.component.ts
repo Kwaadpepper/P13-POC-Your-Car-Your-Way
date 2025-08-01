1939e20c47fc2c703b868fc6b5c5afc3be3e3f9b
Jérémy Munsch <github@jeremydev.ovh>
2025-08-01 18:26:58 +0200

    refactor(frontend-users): update component imports and structure
    - Changed import path for App component in app.component.spec.ts and bootstrap.ts to './app.component'.
    - Created app.component.css and app.component.html files with initial content.
    - Added app.component.ts file with component definition and title signal.

--- frontend/projects/users-app/src/app/app.component.ts
+++ frontend/projects/users-app/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.html',
-  styleUrl: './app.css',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
   imports: [RouterOutlet],
 })
 export class App {

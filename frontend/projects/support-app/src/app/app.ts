f26e46835383abe965c3d13bbe3af165f7fc0dc1
Jérémy Munsch <github@jeremydev.ovh>
2025-08-08 09:29:25 +0200

    refactor(frontend-support): suppression du composant BackButton des imports

--- frontend/projects/support-app/src/app/app.ts
+++ frontend/projects/support-app/src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core'
 import { RouterModule } from '@angular/router'
-import { BackButton } from '@support-shared/components'
 
 @Component({
   selector: 'support-root',
   imports: [
     RouterModule,
-    BackButton,
   ],
   templateUrl: './app.html',
   styleUrl: './app.css',

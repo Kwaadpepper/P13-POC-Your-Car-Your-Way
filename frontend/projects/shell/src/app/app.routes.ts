4ee5376f063823fd6d92c6eda04a0d7c7a9ef28e
Jérémy Munsch <github@jeremydev.ovh>
2025-08-08 06:20:27 +0200

    refactor(app.routes): Ajout de la protection de la route d'accueil avec GuestGuard

--- frontend/projects/shell/src/app/app.routes.ts
+++ frontend/projects/shell/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router'
+import { GuestGuard } from '@shell-core/auth/guards'
 import { HomePage } from '@shell-core/auth/pages/home/home'
 import { authRoutes } from '@shell-core/auth/routes'
 import { homeRoutes } from '@shell-core/home/routes'
@@ -6,6 +7,7 @@ import { homeRoutes } from '@shell-core/home/routes'
 export const routes: Routes = [
   {
     path: '',
+    canActivate: [GuestGuard],
     component: HomePage,
   },
   ...authRoutes,

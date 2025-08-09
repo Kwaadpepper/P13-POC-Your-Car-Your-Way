2105bcd095ef499e40e51c03240f5072e3265ce6
Jérémy Munsch <github@jeremydev.ovh>
2025-08-09 08:39:12 +0200

    fix(frontend): Federation sharing issues fixed by disabling shareAll

--- frontend/shared/federation-shared.config.cjs
+++ frontend/shared/federation-shared.config.cjs
@@ -3,20 +3,26 @@ const { shareAll } = require('@angular-architects/native-federation/config');
 module.exports = {
   shared: {
     '@angular/core': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular/core/primitives/signals': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular/core/primitives/di': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
     '@angular/common': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular/common/http': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular/platform-browser': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular/platform-browser/animations/async': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular/platform-browser-dynamic': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
     '@angular/router': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
     '@angular/forms': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    'rxjs': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    'rxjs/operators': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
     'primeng': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
     'primeicons': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-    ...shareAll({
-      singleton: true,
-      strictVersion: true,
-      requiredVersion: 'auto'
-    }),
+    // ...shareAll({
+    //   singleton: true,
+    //   strictVersion: true,
+    //   requiredVersion: 'auto'
+    // }),
   },
   skip: [
-    /^@primeuix\//,
-    /^@primeng\//,
     'rxjs/ajax',
     'rxjs/fetch',
     'rxjs/testing',

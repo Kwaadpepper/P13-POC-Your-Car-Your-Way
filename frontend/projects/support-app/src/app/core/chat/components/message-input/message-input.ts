6433b78931ebca2486aaca8e201a383548ca35f8
Jérémy Munsch <github@jeremydev.ovh>
2025-08-11 15:50:28 +0200

    refactor(frontend-support): messageInput gestion du timer d'arrêt de saisie

--- frontend/projects/support-app/src/app/core/chat/components/message-input/message-input.ts
+++ frontend/projects/support-app/src/app/core/chat/components/message-input/message-input.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common'
-import { Component, EventEmitter, Output } from '@angular/core'
+import { Component, EventEmitter, OnDestroy, Output } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 
 import { ButtonModule } from 'primeng/button'
@@ -16,18 +16,33 @@ import { TextareaModule } from 'primeng/textarea'
   templateUrl: './message-input.html',
   styleUrl: './message-input.css',
 })
-export class MessageInput {
+export class MessageInput implements OnDestroy {
   @Output() send = new EventEmitter<string>()
   @Output() typing = new EventEmitter<boolean>()
 
   text = ''
   private lastTypingSent = 0
+  private readonly typingDelay = 1800 // ms
+
+  private stopTypingTimer?: number
+  private readonly stopTypingDelay = 2000 // ms
+
+  ngOnDestroy(): void {
+    if (this.stopTypingTimer) {
+      clearTimeout(this.stopTypingTimer)
+      this.stopTypingTimer = undefined
+    }
+  }
 
   submit() {
     const trim = this.text.trim()
     if (!trim) return
     this.send.emit(trim)
     this.text = ''
+    if (this.stopTypingTimer) {
+      clearTimeout(this.stopTypingTimer)
+      this.stopTypingTimer = undefined
+    }
     this.typing.emit(false)
   }
 
@@ -40,11 +55,19 @@ export class MessageInput {
 
   onInput(_: Event) {
     const now = Date.now()
-    if (now - this.lastTypingSent > 800) {
+
+    if (now - this.lastTypingSent > this.typingDelay) {
       this.emitTyping(true)
       this.lastTypingSent = now
-      setTimeout(() => this.typing.emit(false), 2000)
     }
+
+    if (this.stopTypingTimer) {
+      clearTimeout(this.stopTypingTimer)
+    }
+    this.stopTypingTimer = window.setTimeout(() => {
+      this.typing.emit(false)
+      this.stopTypingTimer = undefined
+    }, this.stopTypingDelay)
   }
 
   emitTyping(v: boolean) {

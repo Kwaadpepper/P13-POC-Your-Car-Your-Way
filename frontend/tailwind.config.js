394322ba636ca244b57d70a8f8489102a3f1b89a
Jérémy Munsch <github@jeremydev.ovh>
2025-08-12 08:33:39 +0200

    refactor(frontend): ajout de TailwindBootstrapGrid avec des largeurs maximales de conteneur

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,4 @@
+import TailwindBootstrapGrid from 'tailwind-bootstrap-grid';
 import PrimeUI from 'tailwindcss-primeui';
 
 /** @type {import('tailwindcss').Config} */
@@ -6,6 +7,18 @@ module.exports = {
     "./projects/**/*.{html,ts}"
   ],
   plugins: [
-    PrimeUI
+    PrimeUI,
+    TailwindBootstrapGrid({
+      container_max_widths: [
+        'sm', '540px',
+        'md', '720px',
+        'lg', '960px',
+        'xl', '1140px',
+        '2xl', '1320px',
+      ],
+    }),
   ],
+  corePlugins: {
+    container: false,
+  },
 }

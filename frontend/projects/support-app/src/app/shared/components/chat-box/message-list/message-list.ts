f2cc805eece9af9eb2a101c60dbe63eca8475a75
Jérémy Munsch <github@jeremydev.ovh>
2025-08-13 22:24:35 +0200

    refactor(frontend-support): fix message-list auto scroll

--- frontend/projects/support-app/src/app/shared/components/chat-box/message-list/message-list.ts
+++ frontend/projects/support-app/src/app/shared/components/chat-box/message-list/message-list.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, ElementRef, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core'
 
-import { ScrollPanelModule } from 'primeng/scrollpanel'
+import { ScrollPanel, ScrollPanelModule } from 'primeng/scrollpanel'
 
 import { ChatMessage } from '~support-domains/chat/models'
 import { UUID } from '~ycyw/shared'
@@ -19,7 +19,7 @@ import { Message } from '../message/message'
 export class MessageList implements AfterViewInit, OnChanges {
   @Input() messages: ChatMessage[] = []
   @Input() currentUser!: UUID
-  @ViewChild('panel') panel?: ElementRef
+  @ViewChild('panel') panel?: ScrollPanel
 
   ngAfterViewInit(): void {
     this.scrollToBottom()
@@ -31,8 +31,15 @@ export class MessageList implements AfterViewInit, OnChanges {
 
   private scrollToBottom() {
     setTimeout(() => {
-      const el = document.querySelector('.sp .p-scrollpanel-content')
-      if (el) el.scrollTop = el.scrollHeight
+      const el = this.panel?.contentViewChild as ElementRef<HTMLElement> | undefined
+      if (!el) {
+        console.error('No panel element found to scroll')
+        return
+      }
+      el.nativeElement.scrollIntoView({
+        block: 'end',
+        behavior: 'smooth',
+      })
     })
   }
 }

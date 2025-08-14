8334b040128036194d12dad736073e3144e83807
Jérémy Munsch <github@jeremydev.ovh>
2025-08-14 14:19:27 +0200

    fix(front-shell): nav-menu update support link to use dynamic URL based on user role

--- frontend/projects/shell/src/app/core/layout/nav-menu/nav-menu.viewmodel.ts
+++ frontend/projects/shell/src/app/core/layout/nav-menu/nav-menu.viewmodel.ts
@@ -1,5 +1,6 @@
 import { computed, inject, Injectable } from '@angular/core'
 
+import { Role } from '~shell-shared/enums'
 import { SessionStore } from '~shell-shared/stores'
 
 @Injectable({
@@ -12,4 +13,11 @@ export class NavMenuViewModel {
   private readonly sessionStore = inject(SessionStore)
 
   public readonly loggedIn = computed(() => this.sessionStore.session().isLoggedIn)
+
+  public readonly supportUrl = computed(() => {
+    const session = this.sessionStore.session()
+    return this.loggedIn() && session.user!.role === Role.CLIENT
+      ? '/reservation/support'
+      : '/backoffice/support'
+  })
 }

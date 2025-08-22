e08cd15b66b5d52899b1be406a073298362dad16
Jérémy Munsch <github@jeremydev.ovh>
2025-08-22 16:56:59 +0200

    feat(back-support): ajout du endpoint faqs

---
+++ backend/services/support-service/support-service-application/src/main/java/com/ycyw/support/application/route/faq/ListAllFaqController.java
@@ -0,0 +1,41 @@
+package com.ycyw.support.application.route.faq;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ycyw.shared.ddd.lib.UseCaseExecutor;
+import com.ycyw.support.application.dto.FaqViewDto;
+import com.ycyw.support.application.presenter.FaqPresenter;
+import com.ycyw.support.domain.usecase.faq.GetAllFaq;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+public class ListAllFaqController {
+  private final UseCaseExecutor useCaseExecutor;
+  private final GetAllFaq.GetAllFaqHandler useCaseHandler;
+  private final FaqPresenter presenter;
+  private static final Logger logger = LoggerFactory.getLogger(ListAllFaqController.class);
+
+  public ListAllFaqController(
+      UseCaseExecutor useCaseExecutor,
+      GetAllFaq.GetAllFaqHandler useCaseHandler,
+      FaqPresenter presenter) {
+    this.useCaseExecutor = useCaseExecutor;
+    this.useCaseHandler = useCaseHandler;
+    this.presenter = presenter;
+  }
+
+  @GetMapping("/faqs")
+  public List<FaqViewDto> getCompanyInfo() {
+
+    final var output = useCaseExecutor.execute(useCaseHandler, new GetAllFaq.Input.GetAll());
+
+    logger.info("Company info retrieved: {}", output);
+
+    return presenter.present(output);
+  }
+}
--- backend/shared/src/main/java/com/ycyw/shared/application/Presenter.java
+++ backend/shared/src/main/java/com/ycyw/shared/application/Presenter.java
@@ -1,11 +1,11 @@
 package com.ycyw.shared.application;
 
-public interface Presenter<U extends Record, M> {
+public interface Presenter<O, I> {
   /**
    * Transforms the given model into a record for presentation.
    *
    * @param model the model to present
    * @return the presented record
    */
-  U present(M model);
+  O present(I model);
 }

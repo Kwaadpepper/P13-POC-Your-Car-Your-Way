9f6997c6638921f72eca328f1075dcb76358ba22
Jérémy Munsch <github@jeremydev.ovh>
2025-08-23 09:27:47 +0200

    chore(back-user): mise a jour des stubs users pour avoir un created at

--- backend/services/user-service/user-service-application/src/main/resources/db/migration/R__data_for_test.sql
+++ backend/services/user-service/user-service-application/src/main/resources/db/migration/R__data_for_test.sql
@@ -1,17 +1,17 @@
 INSERT INTO user_context.operators (id, name, email, roles, credential, updated_at, deleted_at)
 VALUES (
-  '00000000-0000-7000-8000-000000000001',
+  '019159f8-d42f-7000-8000-000000000001',
   'Jane Doe Operator',
   'user@example.net',
   '["SUPPORT"]'::jsonb,
-  '00000000-0000-7000-8000-000000000010',
+  '019159f8-d42f-7000-8000-000000000010',
   '2025-08-22 23:05:18.808000',
   NULL
 );
 
 INSERT INTO user_context.credentials (id, last_connection, hashed_identifier, hashed_password, sso_id, sso_provider, topt_code_value, passkey_id, passkey_publickey, passkey_sign_count, passkey_type)
 VALUES (
-  '00000000-0000-7000-8000-000000000010',
+  '019159f8-d42f-7000-8000-000000000010',
   NULL,
   '01332c876518a793b7c1b8dfaf6d4b404ff5db09b21c6627ca59710cc24f696a',
   '$argon2id$v=19$m=16384,t=2,p=1$W8AuZ3AvUE35NMcgKjjlbw$8dZCMU1eMt+2/N2fBnLXBlIiAP+JqAbfMw4sVxmImiY',
@@ -22,7 +22,7 @@ VALUES (
 
 INSERT INTO user_context.clients (id, last_name, first_name, email, phone, birthdate, address_line1, address_line2, address_line3, address_city, address_postcode, address_country, credential, updated_at, deleted_at)
 VALUES (
-  '00000000-0000-7000-8000-000000000002',
+  '019159f8-d42f-7000-8000-000000000002',
   'Doe',
   'John',
   'user@example.net',
@@ -33,14 +33,14 @@ VALUES (
   'Dorseystad',
   '57850',
   'FR',
-  '00000000-0000-7000-8000-000000000020',
+  '019159f8-d42f-7000-8000-000000000020',
   '2025-08-22 23:05:18.238000',
   NULL
 );
 
 INSERT INTO user_context.credentials (id, last_connection, hashed_identifier, hashed_password, sso_id, sso_provider, topt_code_value, passkey_id, passkey_publickey, passkey_sign_count, passkey_type)
 VALUES (
-  '00000000-0000-7000-8000-000000000020',
+  '019159f8-d42f-7000-8000-000000000020',
   NULL,
   '6cea57c2fb6cbc2a40411135005760f241fffc3e5e67ab99882726431037f908',
   '$argon2id$v=19$m=16384,t=2,p=1$qryx/ImtQxg5Zce6qIMJCQ$T4sJ3jS0J7vhuoeBfBwi0VtxOVb1Up3ZbitHwTcbGHQ',

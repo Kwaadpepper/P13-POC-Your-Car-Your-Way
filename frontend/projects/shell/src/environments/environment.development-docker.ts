83b63ca7a68158e61ff2a188167824c20b29044f
Jérémy Munsch <github@jeremydev.ovh>
2025-08-24 21:38:59 +0200

    fix(front-shell): mettre à jour resourceUrl pour utiliser userServiceEndpoint

--- frontend/projects/shell/src/environments/environment.development-docker.ts
+++ frontend/projects/shell/src/environments/environment.development-docker.ts
@@ -1,4 +1,5 @@
 export const environment = {
   env: 'production',
   endpoint: 'http://localhost:8080',
+  userServiceEndpoint: 'http://localhost:8080/user-service-application',
 }
--- frontend/projects/shell/src/environments/environment.development.ts
+++ frontend/projects/shell/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   env: 'development',
   endpoint: 'http://localhost:8080',
+  userServiceEndpoint: 'http://localhost:8080/user-service-application',
 }
